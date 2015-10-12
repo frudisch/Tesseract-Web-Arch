1b2c9e7f2cbc9e671648395328a2710e199255f0
frudisch <FRudisch@gmx.de>
2015-10-12 23:23:39 +0200

    added german training data

--- Tesseract/src/main/java/Main.java
+++ Tesseract/src/main/java/Main.java
@@ -4,20 +4,21 @@ import net.sourceforge.tess4j.TesseractException;
 import java.io.File;
 
 /**
- *
+ * Tesseract training data: https://code.google.com/p/tesseract-ocr/downloads/list
  *
  * Created by Florian on 12.10.2015.
  */
 public class Main {
 
     public static void main(String[] args){
-        File image = new File("./src/main/resources/IMG_20151006_120933.jpg");
+        File image = new File("./Tesseract/src/main/resources/IMG_20151006_120933.jpg");
 
         System.out.println(image.exists());
         System.out.println(new File("./").getAbsolutePath());
 
         Tesseract instance = Tesseract.getInstance();
 
+        instance.setLanguage("deu");
         try {
             String result = instance.doOCR(image);
             System.out.println(result);
